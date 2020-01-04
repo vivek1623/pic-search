197c0842d5e559348a662072ddff4f353fe8cf44
Vivek Vaibhav <vivek@oriserve.com>
2020-01-04 21:01:39 +0530

    reinstalled

---
+++ build/precache-manifest.f9e520cb808fd1d5d3171779a0cbec22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b292ab5128953e16f535cfff95d73b30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2c96358c4ca539480ec",
+    "url": "/static/css/2.95363701.chunk.css"
+  },
+  {
+    "revision": "036bd76ec92fcb238fe5",
+    "url": "/static/css/main.5a5b332f.chunk.css"
+  },
+  {
+    "revision": "a2c96358c4ca539480ec",
+    "url": "/static/js/2.cd098751.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/static/js/2.cd098751.chunk.js.LICENSE"
+  },
+  {
+    "revision": "036bd76ec92fcb238fe5",
+    "url": "/static/js/main.7bd294fd.chunk.js"
+  },
+  {
+    "revision": "829a278a942fdab5fe23",
+    "url": "/static/js/runtime-main.22ae5359.js"
+  }
+]);
