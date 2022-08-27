d64dbe01c1a621cd0be139e02ad37e36fcf1182b
Vivek Vaibhav <vivekvaibhav2324@gmail.com>
2022-08-27 18:22:50 +0530

    Updates

---
+++ precache-manifest.c0b954b25b3339e716ac8a892ee87d34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c125f9c679abc89fcc2c08b74c522caa",
+    "url": "/pic-search/index.html"
+  },
+  {
+    "revision": "d7152fd56d3bc16195d2",
+    "url": "/pic-search/static/css/2.89b1a116.chunk.css"
+  },
+  {
+    "revision": "0d049a0ea713c828c267",
+    "url": "/pic-search/static/css/main.0ca3de28.chunk.css"
+  },
+  {
+    "revision": "d7152fd56d3bc16195d2",
+    "url": "/pic-search/static/js/2.44f89c9d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/pic-search/static/js/2.44f89c9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d049a0ea713c828c267",
+    "url": "/pic-search/static/js/main.e6f6bf76.chunk.js"
+  },
+  {
+    "revision": "78b592564fde4543ce28",
+    "url": "/pic-search/static/js/runtime-main.d5cf95ac.js"
+  }
+]);
